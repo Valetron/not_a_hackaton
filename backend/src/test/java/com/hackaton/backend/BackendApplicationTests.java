13d6fc66f108b5e2e876c8c31fc0aecab1a74666
Andrey <what>
2023-10-13 16:49:36 +0300

    init

---
+++ backend/src/test/java/com/hackaton/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
